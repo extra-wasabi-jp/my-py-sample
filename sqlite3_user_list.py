4e1a308b857ba3e017600f7f6ff686af87e99cf3
Akizawa, Shuji <sakizawa@l-ain.com>
2017-12-16 08:43:17 +0900

    fix unicode

--- sqlite3_user_list.py
+++ sqlite3_user_list.py
@@ -9,7 +9,7 @@ cur = conn.cursor()
 
 for row in cur.execute("SELECT * FROM user ORDER BY user_id;"):
 
-    print u"{user_id}\t{account_id}\t{emp_no}\t{email}\t{password}\t{user_nm}\t{user_nm_kn}\t{user_nm_en}\t{join_dt}".format( \
+    print (u"{user_id}\t{account_id}\t{emp_no}\t{email}\t{password}\t{user_nm}\t{user_nm_kn}\t{user_nm_en}\t{join_dt}".format( \
         user_id=row[0] \
         , account_id=row[1] \
         , emp_no=row[2] \
@@ -18,6 +18,6 @@ for row in cur.execute("SELECT * FROM user ORDER BY user_id;"):
         , user_nm=row[5] \
         , user_nm_kn=row[6] \
         , user_nm_en=row[7] \
-        , join_dt=row[8])
+        , join_dt=row[8]).encode("utf-8"))
 
 conn.close()
